1f5cbaa8f616225c0d3d15bfcb7a37591a4cf99c
cyryll <cyrylpaull@gmail.com>
2021-01-26 13:43:40 +0300

    footer

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Home from './components/pages/Home'
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
 import SignUp from './components/pages/SignUp';
+import Footer from './components/Footer';
 function App() {
   return (
     <div className="App">
@@ -20,6 +21,7 @@ function App() {
           <Route path='/products' component={Products}/>
           <Route path='/signup' component={SignUp}/>
         </Switch>
+        <Footer/>
       </Router>
 
     </div>
